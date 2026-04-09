1e3f0bbccdb300a847f1d318fde15a1a33b7332f
loksuf <zay.mir2008@gmail.com>
2026-04-09 20:00:28 +0300

    first for scripts

---
+++ scripts/bash/compile.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wine ~/files/win/msvc/x64/ml64.exe test.asm /link /entry:main
---
+++ scripts/bash/core/dependencies.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+declare -Ar paths=(
+    ["ml64"]="~/files/win/msvc/x64/ml64.exe"
+    ["key2"]="value2"
+    ["key3"]="value3"
+)
+
+function check_file() {
+  if [[ ! -e "" ]]
+  return 0
+}
